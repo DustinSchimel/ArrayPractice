0c7aa7bd304a033722eea418e3132282af858d94
Dustin Schimel <dustin.schimel@gmail.com>
2018-02-06 13:43:34 -0700

    Created all the base files

---
+++ ArrayPractice/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  ArrayPractice
+//
+//  Created by Schimel, Dustin on 2/6/18.
+//  Copyright © 2018 Schimel, Dustin. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream>
+
+class Controller
+{
+public:
+    Controller();
+    void start();
+};
+
+#endif /* Controller_hpp */
---
+++ ArrayPractice/Controller/Runner.cpp
@@ -0,0 +1,17 @@
+//
+//  Runner.cpp
+//  ArrayPractice
+//
+//  Created by Schimel, Dustin on 2/6/18.
+//  Copyright © 2018 Schimel, Dustin. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+int main()
+{
+    Controller app;
+    app.start();
+    
+    return 0;
+}
