ba9ebe7ced062e62c184e7be65026d536e00107c
Dustin Schimel <dustin.schimel@gmail.com>
2018-02-06 13:50:01 -0700

    Added examples on how to use arrays in C++

--- ArrayPractice/Controller/Controller.cpp
+++ ArrayPractice/Controller/Controller.cpp
@@ -19,5 +19,21 @@ Controller :: Controller()
 
 void Controller :: start()
 {
+    int firstArray [100];
+    double secondArray [5];
+    string thirdArray [10];
     
+    for (int index = 0; index < 100; index++)
+    {
+        firstArray[index] = index;
+    }
+    
+    secondArray[2] = 3.14;
+    
+    thirdArray[0] = "My favorite";
+    
+    thirdArray[10] = "food";
+    
+    cout << thirdArray[0] << " " << thirdArray[10] << " is " << secondArray[2] <<
+    " and my favorite number is " << firstArray[6] << endl;
 }
